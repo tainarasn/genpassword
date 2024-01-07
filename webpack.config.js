3c75a645023dcfa1d6438e8e83664e10b5b87578
Tainara Novaes <tainarasn.sba@gmail.com>
2024-01-07 15:18:25 -0300

    fix: fonts

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+// webpack.config.js
+
+module.exports = {
+    // other webpack configurations...
+    externals: {
+        react: "React", // Tells webpack to use React as an external dependency
+        // Add other external dependencies as needed
+    },
+}
